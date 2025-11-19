7e1483e11bd27804ad8849922de838e27fe96135
Egessa David Wafula <davy.kyute@gmail.com>
2025-11-19 14:04:42 +0300

    Local 1

--- src/components/image-placeholder.tsx
+++ src/components/image-placeholder.tsx
@@ -1,6 +1,9 @@
-import Image from 'next/image';
-import { PlaceHolderImages, type ImagePlaceholder } from '@/lib/placeholder-images';
-import { cn } from '@/lib/utils';
+import Image from "next/image";
+import {
+  PlaceHolderImages,
+  type ImagePlaceholder,
+} from "@/lib/placeholder-images";
+import { cn } from "@/lib/utils";
 
 type ImagePlaceholderProps = {
   id: string;
@@ -9,23 +12,31 @@ type ImagePlaceholderProps = {
   fill?: boolean;
 };
 
-export function ImagePlaceholder({ id, className, imageClassName, fill = false }: ImagePlaceholderProps) {
+export function ImagePlaceholder({
+  id,
+  className,
+  imageClassName,
+  fill = false,
+}: ImagePlaceholderProps) {
   const placeholder = PlaceHolderImages.find((p) => p.id === id);
 
   if (!placeholder) {
     return <div className={cn("bg-muted", className)}></div>;
   }
 
-  const [width, height] = placeholder.imageUrl.split('/').slice(-2);
+  const [width, height] = placeholder.imageUrl.split("/").slice(-2);
 
   return (
     <div className={cn("relative overflow-hidden", className)}>
       <Image
         src={placeholder.imageUrl}
         alt={placeholder.description}
-        data-ai-hint={placeholder.imageHint}
+        height={1000}
+        width={1000}
         className={cn("object-cover", imageClassName)}
-        {...(fill ? { fill: true } : { width: parseInt(width), height: parseInt(height) })}
+        // {...(fill
+        //   ? { fill: true }
+        //   : { width: parseInt(width), height: parseInt(height) })}
       />
     </div>
   );
