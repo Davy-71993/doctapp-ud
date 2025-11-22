be8dbfa2b7a29146c62dace5b498b61269a0ca05
DAVID EGESSA <davy.kyute@gmail.com>
2025-11-22 15:41:15 +0000

    reduce the verticle spacing of all the main sections to max of py-12

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -157,7 +157,7 @@ export default function LandingPage() {
             </div>
         </section>
 
-        <section id="features" className="w-full py-12 md:py-24 lg:py-32">
+        <section id="features" className="w-full py-12">
           <div className="container max-w-7xl mx-auto px-4 md:px-6">
             <div className="flex flex-col items-center justify-center space-y-4 text-center">
               <div className="space-y-2">
@@ -190,7 +190,7 @@ export default function LandingPage() {
           </div>
         </section>
 
-        <section id="partners" className="w-full py-12 md:py-24 lg:py-32 bg-muted">
+        <section id="partners" className="w-full py-12 bg-muted">
           <div className="container max-w-7xl mx-auto px-4 md:px-6">
             <div className="flex flex-col items-center justify-center space-y-4 text-center">
               <div className="space-y-2">
@@ -224,7 +224,7 @@ export default function LandingPage() {
           </div>
         </section>
 
-        <section id="emergency" className="w-full py-12 md:py-24 lg:py-32">
+        <section id="emergency" className="w-full py-12">
           <div className="container max-w-7xl mx-auto px-4 md:px-6">
             <div className="flex flex-col items-center justify-center space-y-4 text-center">
               <div className="space-y-2">
